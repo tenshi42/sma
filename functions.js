077779f8f74fc37ce354bdb05284f18a49d90002
TENSHI42-ULTRAB\tensh <tenshirock42@hotmail.fr>
2018-06-29 16:15:24 +0200

    Main animals behaviors

--- functions.js
+++ functions.js
@@ -1,14 +1,117 @@
-class Lapin{
+var canvasWidth = 600;
+var canvasHeight = 400;
+var canvas = null;
+var ctx = null;
+var rate = 30;
+var loopHandler = null;
+var lapinId = 0;
+var renardId = 0;
+var lapins = [];
+var renards = [];
 
+class Animal {
+  constructor(posX, posY, image) {
+    this.posX = posX;
+    this.posY = posY;
+    this.dirX = 0;
+    this.dirY = 0;
+    this.continuity = 0;
+    this.width = 35;
+    this.height = 35;
+    this.img = new Image();
+    this.img.src = image;
+  }
+
+  move(){
+    if(this.continuity <= 0){
+      this.setDir();
+    }
+    this.posX += this.dirX;
+    this.posY += this.dirY;
+    if(!this.isInBox()){
+      this.posX -= this.dirX;
+      this.posY -= this.dirY;
+      this.setDir();
+    }
+    this.continuity--;
+  }
+
+  draw() {
+    ctx.drawImage(this.img, this.posX, this.posY, this.width, this.height);
+  }
+
+  setDir() {
+    this.continuity = Math.floor(Math.random() * 100);
+    this.dirX = Math.random() * 2 - 1;
+    this.dirY = Math.random() * 2 - 1;
+  }
+
+  isInBox() {
+    return this.posX >= 0 && this.posX <= canvasWidth - this.width && this.posY >= 0 && this.posY <= canvasWidth - this.height;
+  }
+}
+
+class Lapin extends Animal{
+  constructor(posX, posY){
+    super(posX, posY, "images/lapin1.png");
+  }
+}
+
+class Renard extends Animal{
+  constructor(posX, posY){
+    super(posX, posY, "images/renard_repro.png");
+  }
+}
+
+function addLapin() {
+  var x = Math.floor(Math.random() * canvasWidth);
+  var y = Math.floor(Math.random() * canvasHeight);
+  lapins[lapinId++] = new Lapin(x, y);
+}
+
+function addRenard() {
+  var x = Math.floor(Math.random() * canvasWidth);
+  var y = Math.floor(Math.random() * canvasHeight);
+  renards[renardId++] = new Renard(x, y);
+}
+
+function draw() {
+  drawInterface();
+
+  for(var i in lapins){
+    lapins[i].draw();
+  }
+
+  for(var i in renards){
+    renards[i].draw();
+  }
+}
+
+function move() {
+  for(var i in lapins){
+    lapins[i].move();
+  }
+
+  for(var i in renards){
+    renards[i].move();
+  }
+}
+
+function drawInterface() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,canvasWidth,canvasHeight);
 }
 
 window.onload = function () {
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
 
   console.log("Ready !");
-  ctx.fillStyle = "blue";
-  ctx.width = 600;
-  ctx.height = 400;
-  ctx.fillRect(0,0,600,400);
+  ctx.width = canvasWidth;
+  ctx.height = canvasHeight;
+
+  loopHandler = setInterval(function () {
+    move();
+    draw();
+  }, 1000/rate);
 };
