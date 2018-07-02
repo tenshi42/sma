5ca29daade2b67e8fc668a412a013978fad47bed
Maxime Pinardon <pinardon.maxime@gmail.com>
2018-07-02 17:06:15 +0200

    foxtodie

--- functions.js
+++ functions.js
@@ -239,7 +239,7 @@ function move() {
 
 function die(){
   for(var i=0;i<foxToDie.lenght;i++){
-    delete renards[foxToDie[i]]; 
+    delete renards[foxToDie[i]];
   }
   for(var i=0;i<lapinsToDie.lenght;i++){
     delete lapins[lapinsToDie[i]];
